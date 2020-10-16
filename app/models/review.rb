318b52719e68ba3776ebf203edcd839cfd2700b9
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 11:03:20 -0700

    PASS test for review rating test is an integer between 1 and 5

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
 
   # Validations
   validates :title, :author, :content_body, :rating, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
