506ccf8c6d754f1f6f79702bc3511b174cc7b7a9
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 13:48:26 -0700

    Add and PASS integration test for deleting a product

---
+++ spec/features/delete_product_pages_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "the delete a product process" do
+  it "deletes a new product" do
+    visit products_path
+    click_link 'Create new product'
+    fill_in 'Name', :with => 'peking duck'
+    fill_in 'Cost', :with => 5
+    fill_in 'Country of origin', :with => 'China'
+    click_on 'Create Product'
+    click_link 'Peking Duck', match: :first
+    click_link 'Delete product'
+    # Tests flash notice message
+    expect(page).to have_content 'Product successfully deleted'
+    # Tests that item was removed
+    expect(page).to have_no_content 'Peking Duck'
+  end
+end
