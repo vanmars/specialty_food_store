fc67212b53a2384fac186c4837d6df377d7c01d1
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 11:16:06 -0700

    Write and FAIL test for product callback method that titleizes name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,4 +9,9 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
 
+  # Callbacks
+  it('titleizes the name of a product') do
+    product = Product.create({name: "dark chocolate peanut butter cups", cost:8, country_of_origin: "United States"})
+    expect(product.name).to(eq("Dark Chocolate Peanut Butter Cups"))
+  end
 end
