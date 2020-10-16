21d68fe7767f37667e0bb482f81b0ab3bd595c1b
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 09:25:12 -0700

    Add basic review controller methods with flash notices/alerts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,55 @@
 class ReviewsController < ApplicationController
 
   def new
-    
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new
+    render :new
   end
 
-  def create 
-
-  end
-
-  def edit
-
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      flash[:notice] = "Review successfully added!"
+      redirect_to product_path(@product)
+    else
+      flash[:alert] = "There was an error. Please try again."
+      render :new
+    end
   end
 
   def show
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :show
+  end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    render :edit
   end
 
   def update
-
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      flash[:notice] = "Review successfully updated!"
+      redirect_to product_path(@review.product)
+    else
+      flash[:alert] = "There was an error. Please try again."
+      render :edit
+    end
   end
 
   def destroy
-    
+    @review = Review.find(params[:id])
+    @review.destroy
+    flash[:notice] = "Review successfully deleted!"
+    redirect_to product_path(@review.product)
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:author, :content_body, :rating)
+    end
 end
