8cb4337c5aaa07d13597e8fcd84b21c7845bdc95
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 11:02:39 -0700

    Write and FAIL test for review rating muyst be integer between 1 and 5

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -9,4 +9,9 @@ describe Review do
   it { should validate_presence_of :author}
   it { should validate_presence_of :content_body}
   it { should validate_presence_of :rating}
+
+  it { should validate_numericality_of(:rating).only_integer.
+    is_greater_than_or_equal_to(1).
+    is_less_than_or_equal_to(5)
+  }
 end
