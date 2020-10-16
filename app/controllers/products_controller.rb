fd9159cf509c77b44e0212042aca2f5c814ccd0d
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 09:19:23 -0700

    Add basic product controller methods with flash notices/alerts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,57 @@
 class ProductsController < ApplicationController
   
   def index 
-
+    @products = Product.all 
+    render :index
   end
 
   def new
-
+    @product = Product.new
+    render :new
   end
 
   def create 
-
-  end
-
-  def edit
-
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully created!"
+      redirect_to products_path
+    else
+      flash[:alert] = "There was an error. Please try again."
+      render :new
+    end
   end
 
   def show
+    @product = Product.find(params[:id])
+    render :show
+  end
 
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
   end
 
   def update
-
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated!"
+      redirect_to products_path
+    else
+      flash[:alert] = "There was an error. Please try again."
+      render :edit
+    end
   end
 
   def destroy
-
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = "Product successfully deleted!"
+    redirect_to products_path
   end
+
+  private 
+    def product_params
+      params.require(:product).permit(:name, :cost, :country_of_origin)
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
 
   def new
-
+    
   end
 
   def create 
