40140112266ab7597dbbd496134eaf74f85bfd5c
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-16 09:02:54 -0700

    Add controllers for products and reviews

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+
+  def new
+
+  end
+
+  def create 
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    
+  end
+end
